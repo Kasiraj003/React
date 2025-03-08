f75912db44bdf133245c54b729373858765eef83
Kasiraj003 <kasiraj1504@gmail.com>
2025-03-08 16:49:24 +0530

    Dynamic-page-app

---
+++ dynamic-page-app/src/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>You can contact us at kasiraj1504@gmail.com
+        or phone : +91 9701760956
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
